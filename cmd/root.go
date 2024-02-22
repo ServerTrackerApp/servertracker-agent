a12e70951e8f58de39839a02ddead9d953e5af25
Luca Fröschke <luca@froeschke.net>
2024-02-22 19:37:16 +0100

    Use custom config instead of viper

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,9 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/viper"
-	"go.servertracker.net/agent/log"
-	"os"
-
 	"github.com/spf13/cobra"
+	"go.servertracker.net/agent/config"
+	"os"
 )
 
 var rootCmd = &cobra.Command{
@@ -29,36 +26,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-func initConfig() {
-	home, err := os.UserHomeDir()
-	cobra.CheckErr(err)
-
-	viper.AddConfigPath("/etc/servertracker/")
-	viper.AddConfigPath(home)
-	viper.SetConfigName("agent")
-	viper.SetConfigType("yml")
-
-	viper.SetDefault("server_id", "")
-	viper.SetDefault("auth_key", "")
-	viper.SetDefault("interval", 60)
-
-	viper.SetDefault("plugin_location", "/etc/servertracker/plugins")
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		log.Log(fmt.Sprintf("Using config file: %s", viper.ConfigFileUsed()), log.INFO)
-	}
-
-	if _, err := os.Stat(viper.ConfigFileUsed()); os.IsNotExist(err) {
-		fmt.Println("Config file not found, creating it...")
-		fmt.Println("Config file created at:", viper.ConfigFileUsed())
-		err := viper.SafeWriteConfig()
-		if err != nil {
-			fmt.Println("Error creating config file:", err)
-		}
-	}
+	config.Init()
 }
--- config/config.go
+++ config/config.go
@@ -4,32 +4,100 @@
 
 package config
 
-import "github.com/spf13/viper"
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+var (
+	config *Config
+	once   sync.Once
+)
 
 const (
-	DefaultLocation = "/etc/servertracker/agent.yml"
-	PluginLocation  = "/etc/servertracker/plugins"
+	Location       = "/etc/servertracker/agent.yml"
+	PluginLocation = "/etc/servertracker/plugins"
 )
 
 type Config struct {
-	ServerId       string `yaml:"server_id"`
-	AuthKey        string `yaml:"auth_key"`
-	Interval       int    `yaml:"interval"`
-	PluginLocation string `yaml:"plugin_location"`
+	ServerId       string                    `yaml:"server_id"`
+	AuthKey        string                    `yaml:"auth_key"`
+	SyncInterval   int                       `yaml:"sync_interval"`
+	PluginLocation string                    `yaml:"plugin_location"`
+	PluginSettings map[string]PluginSettings `yaml:"plugin_settings"`
 }
 
-func NewConfig() *Config {
+type PluginSettings struct {
+	PluginName string
+	Settings   map[string]interface{}
+}
+
+func newConfig() *Config {
 	return &Config{
-		Interval:       60,
+		ServerId:       "",
+		AuthKey:        "",
+		SyncInterval:   60,
 		PluginLocation: PluginLocation,
+		PluginSettings: make(map[string]PluginSettings),
 	}
 }
 
-func FromViper() *Config {
-	return &Config{
-		ServerId:       viper.GetString("server_id"),
-		AuthKey:        viper.GetString("auth_key"),
-		Interval:       viper.GetInt("interval"),
-		PluginLocation: viper.GetString("plugin_location"),
+func Init() {
+	once.Do(func() {
+		filePath := Location
+		dirPath := filepath.Dir(filePath)
+		if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+			err = os.MkdirAll(dirPath, 0755)
+			if err != nil {
+				panic(err)
+			}
+		}
+		_, err := os.Stat(filePath)
+		if os.IsNotExist(err) {
+			defaultConfig := newConfig()
+			configBytes, _ := yaml.Marshal(defaultConfig)
+			err = os.WriteFile(filePath, configBytes, 0644)
+			if err != nil {
+				panic(err)
+			}
+			config = defaultConfig
+		} else {
+			configBytes, err := os.ReadFile(filePath)
+			if err != nil {
+				panic(err)
+			}
+			err = yaml.Unmarshal(configBytes, &config)
+			if err != nil {
+				panic(err)
+			}
+		}
+	})
+}
+
+func GetConfig() *Config {
+	return config
+}
+
+func SetConfig(c *Config) {
+	config = c
+}
+
+func Save() {
+	filePath := Location
+	configBytes, _ := yaml.Marshal(config)
+	err := os.WriteFile(filePath, configBytes, 0644)
+	if err != nil {
+		panic(err)
 	}
+}
+
+func GetPluginSettings(pluginName string) (PluginSettings, bool) {
+	settings, ok := config.PluginSettings[pluginName]
+	return settings, ok
+}
+
+func SetPluginSettings(pluginName string, settings PluginSettings) {
+	config.PluginSettings[pluginName] = settings
 }
